6027bf0552eca7d4581f5717e2eb3257e34a19ae
“John <john@email.com>
2025-03-07 19:58:40 +0100

    Test

--- utils.py
+++ utils.py
@@ -1,4 +1,3 @@
-def text():
-    n = "Hi, my name is Dima"
-
-return n
+def add_two_numbers(number1: float | str, number2: float | str) -> float:
+    result = float(number1) + float(number2)
+    return result
